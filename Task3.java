e6d03c268c2ece4b5fce8d8d9a72eeba438bee5a
Darshanjaiswal <166190135+Darshanjaiswal@users.noreply.github.com>
2024-04-12 13:55:31 +0530

    Add files via upload

---
+++ Task3.java
@@ -0,0 +1,22 @@
+public class Task3 {
+    public static String removeZero(String str)
+    {
+        int i = 0;
+        while (i < str.length() && str.charAt(i) == '0')
+            i++;
+ 
+        StringBuffer sb = new StringBuffer(str);
+ 
+        sb.replace(0, i, "");
+ 
+        return sb.toString();
+    }
+ 
+    public static void main(String[] args)
+    {
+ 
+        String str = "00000123789";
+        str = removeZero(str);
+        System.out.println(str);
+    }
+}
